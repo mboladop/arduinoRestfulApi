1bd44d136215b41e9c03df85ae16428c1aa37800
Fernando Lafuente Saiz <>
2020-12-21 00:25:47 +0100

    Avoid CORS

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // Set custom headers for CORS
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 const db = new sqlite3.Database('./readings.db', (err) => {
     if (err) {
         console.error("Error opening database " + err.message);
